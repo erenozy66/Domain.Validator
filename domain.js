b5ff8b0a7e386e23427b1ee6f7369e10aad1d8a2
erenozy66 <eren_ozyurek@outlook.com>
2023-11-17 14:52:37 +0100

    updated JS

--- domain.js
+++ domain.js
@@ -1,21 +1,32 @@
-let orginalurl = 'https://domain-checker7.p.rapidapi.com/whois?domain=bol.com';
-let url = orginalurl.replace('bol.com', document.getElementById("domainname").innerHTML);
-console.log(url);
+
+
+let orginalurl = 'https://domain-checker7.p.rapidapi.com/whois?domain=';
+
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'bcd1f3a04dmsh3328dc823586677p11a6c0jsne0f06219c9ec',
+        'X-RapidAPI-Key': '0e42139164msh4a8ec9a4db9b9b3p1b59c9jsnbf29f81cfb5a',
         'X-RapidAPI-Host': 'domain-checker7.p.rapidapi.com'
     }
 };
 
 async function main() {
-try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-} catch (error) {
-    console.error(error);
-}
-}
-console.log(main());
+		try {
+			let geturl = retrieve();
+			let url = orginalurl+geturl;
+			const response = await fetch(url, options);
+			const result = await response.text();
+			console.log(result);
+		} catch (error) {
+			console.error(error);
+		}
+ 	}
+
+
+
+   const retrieve = () => {
+	let myInput = document.getElementById('domainname').value;
+	return myInput;
+   }
+
+	document.getElementById('submitval').addEventListener('click', main);
