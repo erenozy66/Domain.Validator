a0b638350c8d6437f2772c07e437eeda919ae32c
erenozy <ozy.066@outlook.com>
2023-11-20 23:33:52 +0100

    Finishing touches

--- domain.js
+++ domain.js
@@ -5,10 +5,12 @@ let orginalurl = 'https://domain-checker7.p.rapidapi.com/whois?domain=';
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'abe322ebfamshcba91590a65ff94p1e49dcjsn1ddf3a572dca',
+        'X-RapidAPI-Key': 'ea222e7d08mshdb97d0dcb949165p1f8119jsn878144c8814f',
         'X-RapidAPI-Host': 'domain-checker7.p.rapidapi.com'
     }
 };
+
+
 
 async function main() {
 		try {
@@ -16,6 +18,7 @@ async function main() {
 			let url = orginalurl+geturl;
 			const response = await fetch(url, options);
 			const result = await response.json();
+			JSON.stringify(result);
 			console.log(result);
 			let expired = result.expires_at;
 			console.log(expired);
@@ -41,9 +44,34 @@ async function main() {
   
 	document.getElementById('submitval').addEventListener('click', main);
 
-	function myFunction(){
+	async function myFunction(){
+		
+	let geturl = retrieve();
+	let url = orginalurl+geturl;
+	const response = await fetch(url, options);
+	const result = await response.json();
+	JSON.stringify(result);
+	let expired = result.expires_at;
+	let domain5 = result.domain;
+	let valid = result.valid;
+	let updated = result.updated_at;
+	let availabilty = result.available;
+	let register = result.registrar;
 	let names = document.getElementById('domainname').value;
 	document.getElementById('span2').textContent= names;
+	document.getElementById('domain5').textContent= domain5;
+	document.getElementById('valid').textContent= valid;
+	document.getElementById('updated').textContent= updated;
+	document.getElementById('registrar').textContent= register;
+	document.getElementById('expired').textContent= expired;
+		if (availabilty) { 
+			document.getElementById('span3').textContent= 'is available'
+			document.getElementById('span3').style.color = '#35FF69';
+			
+		} else { document.getElementById('span3').textContent= 'is not available';
+				document.getElementById('span3').style.color = '#9A031E';
+		}
+	
 	document.getElementById("myAnchor").href = "https://www." + retrieve();
 	
 	
